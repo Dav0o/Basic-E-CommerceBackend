93c8fb0c8e9f7ec014722c5e2bff9d5c959d27de
reivaJSnevet <128876020+reivaJSnevet@users.noreply.github.com>
2023-10-19 01:06:41 -0600

    Post del post a api externa

--- E-CommerceBackend/Program.cs
+++ E-CommerceBackend/Program.cs
@@ -1,26 +1,26 @@
 using Application;
 using Domain;
-
-using System.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using System.Text.Json.Serialization;
+using static Microsoft.AspNetCore.Builder.WebApplication;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Add External Dependencies
+// Add External Dependencies
 builder.Services.AddDomain(builder.Configuration);
 builder.Services.AddServices(builder.Configuration);
 
+// Add HttpClientFactory
+builder.Services.AddHttpClient();
 
 // Add services to the container.
-
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
-
-
-    //con este ignoramos las referencias circulares y no se muestran en el json
+    // Con este ignoramos las referencias circulares y no se muestran en el JSON
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
-
 });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -36,9 +36,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
